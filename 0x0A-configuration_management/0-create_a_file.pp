75887712c3e6bfbd2bba95c0ee9d6eb7d680c7f5
midunodunsi <midun99.odunsi@gmail.com>
2024-05-29 15:25:54 -0700

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file
-file { '/tmp/school'
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet'
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
 
