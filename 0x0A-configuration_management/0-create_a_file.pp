2364cf95f548caf3109f76a8c7f09d052eb929cd
midunodunsi <midun99.odunsi@gmail.com>
2024-05-29 14:24:17 -0700

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# creates a file
+file { '/tmp/school'
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
+	content => 'I love Puppet'
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#!/usr/bin/pup
+package {'flask':
+	ensure   => '2.1.0',
+	provider => 'pip3'
 }
