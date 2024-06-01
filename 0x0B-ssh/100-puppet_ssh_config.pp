9e4ac70d3e5eea7ee2317edcb5f726bb48cdc6bc
midunodunsi <midun99.odunsi@gmail.com>
2024-06-01 18:15:16 +0100

    config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
+# seeting up ssh config for client
 
-include stdlib
-
-file_line { 'SSH Private Key':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  replace            => true,
-  append_on_no_match => true
+# Setting up ssh config for client
+file_line { 'turn of pass auth':
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
 }
 
-file_line { 'Deny Password Auth':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  replace            => true,
-  append_on_no_match => true
+file_line { 'declare identity file (pub key)':
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+
 }
