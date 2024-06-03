99fbeb63d3ffbfc7df39db7de588ab9d5bd48904
midunodunsi <midun99.odunsi@gmail.com>
2024-06-03 19:05:47 +0100

    data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Returns to-do list information for a given employee ID.
+"""
+import re
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Base URL for the JSONPlaceholder API
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # Get the employee information using the provided employee ID
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
+
+    # Get the to-do list for the employee using the provided employee ID
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params=params).json()
+
+    # Filter completed tasks and count them
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    # Print the employee's name and the number of completed tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+    user.get("name"), len(completed), len(todos)))
+
+    for complete in completed:
+                print("\t {}".format(complete))
