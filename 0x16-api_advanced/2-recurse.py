3f970a45958859a901195388b9a5542cf476236b
midunodunsi <midun99.odunsi@gmail.com>
2024-06-22 02:49:08 +0100

    pycodestyle

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,51 @@
 #!/usr/bin/python3
-"""Contains recurse function"""
+"""
+Script to query a list of all hot posts on a given Reddit subreddit.
+"""
 
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
+    """
+    Recursively retrieves a list of titles of all hot posts
+    on a given subreddit.
+    """
+    # Construct the URL for the subreddit's hot posts in JSON format
+
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    # Define headers for the HTTP request, including User-Agent
     headers = {
-    "User-Agent": "0x16-api_advanced:project:\
-            v1.0.0 (by /u/firdaus_cartoon_jr)"
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
             }
+
+    # Define parameters for the request, including pagination and limit
     params = {
             "after": after,
             "count": count,
             "limit": 100
-                                                        }
-    response = requests.get(url, headers=headers, params=params,
-                                                            allow_redirects=False)
+            }
+
+    # Send a GET request to the subreddit's hot posts page
+    response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False)
+
+    # Check if the response status code indicates a not-found error (404)
     if response.status_code == 404:
         return None
-
+    # Parse the JSON response and extract relevant data
     results = response.json().get("data")
     after = results.get("after")
     count += results.get("dist")
+
+    # Append post titles to the hot_list
     for c in results.get("children"):
         hot_list.append(c.get("data").get("title"))
 
+    # If there are more posts to retrieve, recursively call the function
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
+
+    # Return the final list of hot post titles
     return hot_list
